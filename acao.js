606df35b8b44e2edc36706495a4d39aad8897916
ranyacmp <ranyacmp>
2023-03-07 16:20:39 -0300

    Adicionando e estilizando o formulario

--- acao.js
+++ acao.js
@@ -1,1 +1,7 @@
+var adicionar= document.querySelector("#adccandidato");
+var questionario= document.querySelector(".questionario");
 
+function mostrar(){
+    questionario.style.display= "block";
+}
+adicionar.addEventListener("click",mostrar);
