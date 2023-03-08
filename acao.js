987bf103142ff5b41746f982084d9a8c5cd0b75d
ranyacmp <ranyacmp>
2023-03-08 14:37:19 -0300

    Adicionando design ao campo candidato

--- acao.js
+++ acao.js
@@ -14,4 +14,27 @@ function ocultar(){
 }
 fechar.addEventListener("click",ocultar);
 
-// -----------------------------------------------------------//
+// ----------------------------------------------------//
+
+
+
+// --------------- ADICIONAR CANDIDATO-----------------// 
+
+var adcCandidato= document.querySelector("#adicionar");
+
+
+
+function teste(){
+    var sp1= document.createElement("div");
+    sp1.className ="candidato";
+
+    var sp2= document.querySelector(".candidato");
+    var divPai= sp2.parentNode;
+
+    divPai.insertBefore(sp1,sp2);
+
+    questionario.style.display= "none";
+}
+adcCandidato.addEventListener("click", teste);
+
+// ----------------------------------------------------//
