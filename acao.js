db3d60448e1aed4d4306dd71e86cf7295414edde
ranyacmp <ranyacmp>
2023-03-09 14:16:02 -0300

    Configurando adicionar alunos e mensagem de teste em formulario

--- acao.js
+++ acao.js
@@ -7,53 +7,100 @@ function mostrar(){
 }
 btnform.addEventListener("click",mostrar);
 
+
+// --------------- TESTA SE P FORMULÁRIO FOI PREENCHIDO -----------------// 
+
+var btnadicionar= document.querySelector("#adicionar");
+var rep= document.querySelectorAll(".input");
+var br1= document.querySelectorAll(".br");
+
+
+// --------------- ADICIONAR UMA DIV DEPOIS DO PREENCHIMENTO DO FORMULÁRIO-----------------// 
+
+
+function teste(){
+
+    if(rep[0].value != "" && rep[1].value != "" && rep[2].value != ""){
+    
+        let sp1= document.createElement('div');
+        sp1.className= "info";
+
+        let sp2= document.querySelector("#candidato");
+        var divPai= sp2.parentNode;
+
+        (divPai.insertBefore(sp1,sp2)).style.display="block";
+
+        questionario.style.display= "none";
+
+    }else{
+
+        var titulo= document.querySelectorAll(".titulo");
+
+        if(rep[0].value == ""){
+
+            let t1= document.createElement('p');
+            t1.className="alerta";
+            t1.innerText= "Inserir nome do representante"
+
+            var divPai2= titulo[1].parentNode;
+
+            divPai2.insertBefore(t1,titulo[1]);
+
+            br1[0].style.display= "none";
+
+        }
+        if(rep[1].value == ""){
+
+            let t3= document.createElement('p');
+            t3.className="alerta";
+            t3.innerText= "Inserir nome do vice-representante"
+
+            
+            var divPai3= titulo[2].parentNode;
+
+            divPai3.insertBefore(t3,titulo[2]);
+
+            br1[1].style.display= "none";
+
+        }
+        if(rep[2].value == ""){
+
+            let t4= document.createElement('p');
+            t4.className="alerta";
+            t4.innerText= "Inserir número"
+
+            var titulo2= document.querySelector("#adicionar");
+            var divPai4= titulo2.parentNode;
+
+            divPai4.insertBefore(t4,titulo2);
+
+            br1[2].style.display= "none";
+
+        }
+       
+
+    
+    }
+}
+btnadicionar.addEventListener("click", teste);
+
+// --------------- FECHARQUESTIONÁRIO ---------------//
+
 var fechar= document.querySelector("#fecharquestionario");
 
 function ocultar(){
     questionario.style.display= "none";
+    var alertas= document.querySelectorAll(".alerta");
+
+    for(var i=0; i < 3; i++){
+        alertas[i].remove();
+    }
+
+    for(var i=0; i < 3; i++){
+        br1[i].style.display= "block";
+    }
 }
 fechar.addEventListener("click",ocultar);
-
-// ----------------------------------------------------//
-
-var btnadicionar= document.querySelector("#adicionar");
-
-var rep= document.getElementById("#rep");
-var vicrep= document.querySelector("#vicerep");
-var numero= document.querySelector("#num");
-
-// function testeForm(){
-//     if(rep.value != ''){
-//         alert("Enviado");
-//     }else{
-//         alert("Preencha o campo vazio!");
-//     }
-// }
-
-
-// --------------- ADICIONAR CANDIDATO-----------------// 
-
-// var adcCandidato= document.querySelector("#adicionar");
-
-function teste(){
-    var sp1= document.createElement("div");
-    sp1.className ="candidato";
-
-    var sp2= document.querySelector(".candidato");
-    var divPai= sp2.parentNode;
-
-    divPai.insertBefore(sp1,sp2);
-
-    questionario.style.display= "none";
-
-    console.log(rep.value);
-    console.log(vicrep.innerText);
-    console.log(numero.value);
-    
-    
-}
-btnadicionar.addEventListener("click", teste);
-
 
 
 
